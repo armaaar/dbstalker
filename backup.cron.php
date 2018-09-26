1b10d2eac2cd5e796fde34eff2cea07907cbcc9c
Ahmed Rafik <ahmedrafik_maaar@outlook.com>
2018-09-26 12:33:47 +0200

    Create a cronjob script to take periodic backups

---
+++ backup.cron.php
@@ -0,0 +1,7 @@
+<?php
+include_once './core/stalker_configuration.core.php';
+include_once './core/stalker_database.core.php';
+include_once './core/stalker_information_schema.core.php';
+include_once './core/stalker_backup.core.php';
+
+Stalker_Backup::create_backup();
